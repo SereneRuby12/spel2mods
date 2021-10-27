d840adddefba0c0764d55087a5dd476e2d553b00
estebanfer <estebanfer2004@gmail.com>
2021-10-27 01:01:21 -0300

    removed unnecesary things, commented messpects

--- bumblebee.lua
+++ bumblebee.lua
@@ -56,11 +56,11 @@ end
 
 local function set_bumblebees_from_previous(companions)
     for i, info in ipairs(bumblebees_t_info) do
-        messpect(info)
+        --messpect(info)
         for ip,p in ipairs(players) do
-            messpect(p.inventory.player_slot, info.slot)
+            --messpect(p.inventory.player_slot, info.slot)
             if p.inventory.player_slot == info.slot then
-                messpect('trueslot')
+                --messpect('trueslot')
                 if info.mounted then
                     local bee = p:topmost_mount()
                     bumblebees[bee.uid] = new_bumblebee(bee)
@@ -72,12 +72,12 @@ local function set_bumblebees_from_previous(companions)
         end
     end
     for i, uid in ipairs(companions) do
-        messpect(uid)
+        --messpect(uid)
         local ent = get_entity(uid)
         for _, info in pairs(bumblebees_t_info_hh) do
-            messpect(ent.type.id, info.e_type, ent.health, info.hp, test_flag(ent.more_flags, ENT_MORE_FLAG.CURSED_EFFECT), info.cursed, ent:is_poisoned(), info.poisoned)
+            --messpect(ent.type.id, info.e_type, ent.health, info.hp, test_flag(ent.more_flags, ENT_MORE_FLAG.CURSED_EFFECT), info.cursed, ent:is_poisoned(), info.poisoned)
             if ent.type.id == info.e_type and ent.health == info.hp and test_flag(ent.more_flags, ENT_MORE_FLAG.CURSED_EFFECT) == info.cursed and ent:is_poisoned() == info.poisoned then
-                messpect('setBEE')
+                --messpect('setBEE')
                 local bee = ent:get_held_entity()
                 bumblebees[bee.uid] = new_bumblebee(bee)
             end
@@ -85,17 +85,12 @@ local function set_bumblebees_from_previous(companions)
     end
 end
 
-local function spawn_bumblebee(x, y, l)
-    local uid = spawn(ENT_TYPE.MOUNT_TURKEY, x, y, l, 0, 0)
-    bumblebees[uid] = new_bumblebee(get_entity(uid))
-end
-
 local function reset_rider_jumps(rider, backitem, backitem_type, jumps)
-    messpect('reload jumps')
+    --messpect('reload jumps')
     if backitem_type == ENT_TYPE.ITEM_TELEPORTER_BACKPACK then
         backitem.teleport_number = jumps
     elseif backitem_type == ENT_TYPE.ITEM_VLADS_CAPE then
-        messpect('vlads ', jumps)
+        --messpect('vlads ', jumps)
         if jumps == 0 then
             backitem.can_double_jump = true
         end
@@ -105,7 +100,6 @@ end
 local function spawn_bumblebee(x, y, l)
     local uid = spawn(ENT_TYPE.MOUNT_TURKEY, x, y, l, 0, 0)
     bumblebees[uid] = new_bumblebee(get_entity(uid))
-    message('spawned')
     return uid
 end
 
@@ -132,11 +126,6 @@ set_callback(function(room_gen_ctx)
 end, ON.POST_ROOM_GENERATION)
 
 set_callback(function()
-    --[[local turkeys = get_entities_by_type(ENT_TYPE.MOUNT_TURKEY)
-    for i, uid in ipairs(turkeys) do
-        --get_entity(uid):set_texture(bumblebee_texture)
-        bumblebees[uid] = new_bumblebee(uid)
-    end]]
     local px, py, pl = get_position(players[1].uid)
     local companions = get_entities_at(0, MASK.PLAYER, px, py, pl, 2)
     set_bumblebees_from_previous(companions)
@@ -146,12 +135,10 @@ set_callback(function()
     --bee spawn on beehives
     local chance = 1
     local beehives = get_entities_by(ENT_TYPE.FLOORSTYLED_BEEHIVE, MASK.ANY, LAYER.FRONT)
-    messpect("beehives:", #beehives)
     for _,uid in ipairs(beehives) do
         if math.random() <= chance then
             local x, y, l = get_position(uid)
             if #get_entities_at(0, MASK.FLOOR, x, y+1, LAYER.FRONT, 0.5) == 0 then
-                messpect("spawned")
                 spawn_bumblebee(x, y+1, l)
                 chance = chance/5
             end
@@ -183,24 +170,18 @@ set_callback(function()
             end
             if bumblebee.rider_uid == -1 then
                 if not c_ent.not_stolen then
-                    messpect(-1, c_ent.not_stolen)
-                    --return_input(c_ent.rider_uid)
                     c_ent.not_stolen = true
-                    messpect(c_ent.not_stolen, "c_ent.not_stolen")
                     c_ent.rider_uid = -1
                 end
             else
                 local rider = get_entity(bumblebee.rider_uid)
-                --messpect(true, rider.uid, c_ent.not_stolen)
-                c_ent.rider_uid = bumblebee.rider_uid
                 if c_ent.not_stolen then
-                    messpect('STOLEN')
+                    --messpect('STOLEN')
                     --steal_input(rider.uid)
                     c_ent.not_stolen = false
                     c_ent.flying = false
                 end
-                local input = read_input(rider.uid)--read_stolen_input(rider.uid)
-                --messpect(input)
+                local input = read_input(rider.uid)
                 if c_ent.flying then
                     bumblebee.velocityy = 0.0001
                     c_ent.flying_timer = c_ent.flying_timer - 1
@@ -209,11 +190,9 @@ set_callback(function()
                         bumblebee.color.b = c_ent.flying_timer*0.005 + 0.5
                     end
                     if c_ent.flying_timer == 0 or bumblebee.state ~= 8 and bumblebee.state ~= 9 and (bumblebee.standing_on_uid == -1 or not test_flag(input, UP_DIR)) then
-                        messpect(bumblebee.state)
                         c_ent.flying = false
                         local backitem = get_entity(rider:worn_backitem())
                         if backitem then
-                            messpect('backitem')
                             reset_rider_jumps(rider, backitem, backitem.type.id, c_ent.rider_jumps)
                         end
                         bumblebee.flags = clr_flag(bumblebee.flags, ENT_FLAG.NO_GRAVITY)
@@ -230,10 +209,8 @@ set_callback(function()
                     hitbx.right = hitbx.right - 0.1
                     bumblebee.x = bumblebee.x+(math.random()/10-0.05)
                     if #get_blocks(get_entities_overlapping_hitbox(0, MASK.FLOOR | MASK.ACTIVEFLOOR, hitbx, bumblebee.layer)) == 0 then
-                        --messpect('notOverlapping')
                         bumblebee.y = bumblebee.y+(math.random()/10-0.05)+y
                     else
-                        --messpect('Overlapping')
                         bumblebee.y = bumblebee.y+(math.random()/20-0.05) + (y < 0 and y or 0)
                     end
                 end
@@ -242,7 +219,6 @@ set_callback(function()
                         c_ent.flying = not c_ent.flying
                         local backitem_uid = rider:worn_backitem()
                         if c_ent.flying then
-                            messpect('pressed', c_ent.flying)
                             bumblebee.velocityy = 0.01
                             --bumblebee.can_doublejump = true
                             bumblebee.flags = set_flag(bumblebee.flags, ENT_FLAG.NO_GRAVITY)
@@ -254,7 +230,6 @@ set_callback(function()
                                 elseif c_ent.rider_jumps == -1 then
                                     if backitem_type == ENT_TYPE.ITEM_TELEPORTER_BACKPACK then
                                         --add save teleport number
-                                        messpect('jumps reset', c_ent.rider_jumps, backitem.teleport_number)
                                         c_ent.rider_jumps = backitem.teleport_number --done?
                                         backitem.teleport_number = 3
                                     elseif backitem_type == ENT_TYPE.ITEM_VLADS_CAPE then
@@ -282,14 +257,12 @@ set_callback(function()
                 else
                     c_ent.rider_jumped = false
                     if not c_ent.flying and c_ent.flying_timer ~= 0 then
-                        messpect('canDoubleJump')
                         bumblebee.can_doublejump = true
                     end
                 end
                 if test_flag(input, WHIP) and rider.holding_uid == -1 or bumblebee.state == 5 then
                     input = clr_flag(input, WHIP)
                 end
-                --messpect(bumblebee.some_state, c_ent.flying, bumblebee.state, c_ent.flying_timer, input)
             end
         else
             bumblebees[uid] = nil
@@ -303,25 +276,24 @@ set_callback(function()
         for uid, c_ent in pairs(bumblebees) do
             local bumblebee = get_entity(uid)
             local holder = bumblebee:topmost()
-            messpect('bee', uid, holder.uid)
+            --messpect('bee', uid, holder.uid)
             if bumblebee ~= holder and (holder.type.search_flags == MASK.PLAYER or holder.type.search_flags == MASK.MOUNT) then -- the bumblebee is being held, and the holder is a player
                 if holder.type.search_flags == MASK.MOUNT then --when the mount is held and the holder is mounted on another, the topmost becomes the mounted
-                    messpect("holder is mount", holder.uid)
+                    --messpect("holder is mount", holder.uid)
                     holder = get_entity(holder.rider_uid)
-                    messpect(bumblebee.rider_uid)
                 end
-                messpect('holding')
+                --messpect('holding')
                 if holder.inventory.player_slot == -1 then
-                    messpect('hh')
+                    --messpect('hh')
                     set_transition_info_hh(holder.type.id, holder.health, test_flag(holder.more_flags, ENT_MORE_FLAG.CURSED_EFFECT), holder:is_poisoned())
                 else
                     set_transition_info(holder.inventory.player_slot, false) --the bumble
                 end
             elseif bumblebee.rider_uid ~= -1 then
                 holder = get_entity(bumblebee.rider_uid)
-                messpect(holder.uid)
+                --messpect(holder.uid)
                 if holder.type.search_flags == MASK.PLAYER then
-                    messpect('mounting')
+                    --messpect('mounting')
                     set_transition_info(holder.inventory.player_slot, true)
                 end
             end
