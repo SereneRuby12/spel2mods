9b93cfb2bcd68c7285892e76a75f2954f126c164
estebanfer <estebanfer2004@gmail.com>
2023-05-02 18:17:34 -0300

    add Full map mode, changes on settings, add meta, etc

--- minimap/minimap.lua
+++ minimap/minimap.lua
@@ -1,16 +1,105 @@
+---@diagnostic disable-next-line: lowercase-global
+meta = {
+  author = "Estebanfer",
+  name = "Mini Map",
+  description = "Adds a simple mini map",
+  version = "0.8",
+}
+
 local map_screen_x, map_screen_y = .0, .0
 local map_screen_size = .0
-local map_size = 25
+local map_size_x, map_size_y = 25, 25
 local map_alpha = 150
 
-local window_open = true
+---@enum MAP_TYPE
+local MAP_TYPE = {
+  FOLLOW_PLAYER = 1,
+  FULL_MAP = 2,
+}
+
+---@class WindowInfo
+---@field x number
+---@field y number
+---@field w number
+---@field h number
+
+---@class ModOptions
+---@field map_alpha integer
+---@field refresh_modulo integer
+---@field all_players boolean
+---@field draw_border boolean
+---@field window_open boolean
+---@field map_type MAP_TYPE
+---@field map_size_x integer
+---@field map_size_y integer
+---@field window_info WindowInfo
+local default_options = {
+  map_alpha = 120,
+  map_size_x = 25,
+  map_size_y = 25,
+  refresh_modulo = 1,
+  all_players = true,
+  draw_border = true,
+  window_open = true,
+  map_type = MAP_TYPE.FOLLOW_PLAYER,
+  window_info = { x = -0.96, y = 0.732, w = 0.2, h = 0.0 }
+}
+
+---@type ModOptions
+---@diagnostic disable-next-line: lowercase-global
+options = { table.unpack(default_options) }
+
 register_option_int("map_alpha", "Map Opacity", "", 120, 1, 255)
-register_option_int("refresh_modulo", "Refresh map 1/x of the frames", "Try increasing to 2-4 if you have performance issues", 1, 1, 10)
+register_option_int("refresh_modulo", "Refresh map 1/x of the frames", "Try increasing this if you have performance issues", 1, 1, 10)
 register_option_bool("all_players", "Reveal map from all players' positions", "Other players will also reveal map tiles. Might have effects on performance", true)
 register_option_bool("draw_border", "Draw map border", "", true)
-register_option_button("open_window", "Move map", "Open a window that allows to move the map position and change size. You can close it after editing it", function ()
-	window_open = true
+register_option_button("open_window", "Move map", "Open a window that allows to move the map position and change size. You can close it after editing", function ()
+	options.window_open = true
 end)
+register_option_button("reset_options", "Reset settings", "", function ()
+  for key, value in pairs(default_options) do
+    options[key] = value
+  end
+end)
+register_option_callback("map_type_stuff", false, function (draw_ctx)
+	options.map_type = draw_ctx:win_combo("Map type", options.map_type, "Follow player\0Full map\0\0") --[[@as MAP_TYPE]]
+  if options.map_type == MAP_TYPE.FOLLOW_PLAYER then
+    draw_ctx:win_indent(20.0)
+    draw_ctx:win_text("Map size, distance from the center of the camera in tiles")
+  	options.map_size_x = draw_ctx:win_drag_int("Horizontal map size", options.map_size_x, 1, 50)
+  	options.map_size_y = draw_ctx:win_drag_int("Vertical map size", options.map_size_y, 1, 50)
+    draw_ctx:win_indent(-20.0)
+  end
+end)
+-- register_option_combo("map_type", "Map type", "", "Follow player\0Full map\0\0", 2)
+
+set_callback(function(save_ctx)
+  local saved_options = json.encode({options})
+  save_ctx:save(saved_options)
+end, ON.SAVE)
+
+set_callback(function(load_ctx)
+  local loaded_options_str= load_ctx:load()
+  if loaded_options_str ~= "" then
+      options = json.decode(loaded_options_str)[1]
+  end
+  --Make it work when new options are added
+  for key, def_val in pairs(default_options) do
+    if options[key] == nil then
+      options[key] = def_val
+    end
+  end
+  map_screen_x, map_screen_y = options.window_info.x, options.window_info.y
+  map_screen_size = options.window_info.w
+end, ON.LOAD)
+-- register_option_callback("", false, function (draw_ctx)
+--   options.map_alpha = draw_ctx:win_slider_int("map_type", options.map_alpha, 1, 255)
+--   options.map_size_x = draw_ctx:win_slider_int("map_size_x", 25, 1, 50)
+--   options.map_size_y = draw_ctx:win_slider_int("map_size_y", 25, 1, 50)
+--   options.refresh_modulo = draw_ctx:win_slider_int("refresh_modulo", 1, 1, 10)
+--   options.all_players = draw_ctx:win_check("all_players", "Reveal map from all players' positions", "Other players will also reveal map tiles. Might have effects on performance", true)
+--   options.draw_border = draw_ctx:win_check("draw_border", "Draw map border", "", true)
+-- end)
 local TILE_TYPE = {
   UNEXPLORED = 0,
   AIR = 1,
@@ -92,13 +181,20 @@ end
 local last_time = -1
 
 set_callback(function()
-    last_time = -1
-    map = {}
-    draw_map = {}
-    for y = 0, CONST.MAX_TILES_VERT do
-      map_front[y] = {}
-      map_back[y] = {}
-    end
+  last_time = -1
+  map = {}
+  draw_map = {}
+  if options.map_type == MAP_TYPE.FULL_MAP then
+    map_size_x = math.floor(((state.width * CONST.ROOM_WIDTH + 6) + 0.5) / 2)
+    map_size_y = math.floor(((state.height * CONST.ROOM_HEIGHT + 6) + 0.5) / 2)
+  elseif options.map_type == MAP_TYPE.FOLLOW_PLAYER then
+    map_size_x = options.map_size_x
+    map_size_y = options.map_size_y
+  end
+  for y = 0, CONST.MAX_TILES_VERT do
+    map_front[y] = {}
+    map_back[y] = {}
+  end
 end, ON.POST_LEVEL_GENERATION)
 
 local function update_map(local_map, left, top, right, bottom)
@@ -166,20 +262,26 @@ set_callback(function()
   end
   local max_x, max_y = state.width * CONST.ROOM_WIDTH, state.height * CONST.ROOM_HEIGHT
   local remainder_max_y = 120 - max_y
-  local cam_x, cam_y = math.floor(state.camera.calculated_focus_x+.5), math.floor(state.camera.calculated_focus_y+.5)
+  local cam_x, cam_y
+  if options.map_type == MAP_TYPE.FOLLOW_PLAYER then
+    cam_x, cam_y = math.floor(state.camera.calculated_focus_x+.5), math.floor(state.camera.calculated_focus_y+.5)
+  elseif options.map_type == MAP_TYPE.FULL_MAP then
+    local left, top, right, bottom = get_bounds()
+    cam_x, cam_y = math.floor((left + right) / 2), math.floor((top + bottom) / 2)
+  end
   draw_map = {}
   local last_draw_map_index = 1
-  for x = -map_size, map_size do
+  for x = -map_size_x, map_size_x do
     local forming_column_start_y = 0
     local forming_column_tile = nil
-    for y = map_size, -map_size, -1 do
-      local grid_x, grid_y = math.floor(cam_x+x+0.5), math.floor(cam_y+y+0.5)
+    for y = map_size_y, -map_size_y, -1 do
+      local grid_x, grid_y = cam_x+x, cam_y+y
       if state.theme == THEME.COSMIC_OCEAN then
         grid_x, grid_y = ((grid_x - 3) % max_x) + 3, ((grid_y-remainder_max_y - 3) % max_y) + 3 + remainder_max_y
       end
       if map[grid_y] and forming_column_tile ~= map[grid_y][grid_x] then
         if forming_column_tile ~= nil then
-          draw_map[last_draw_map_index] = {x = x+map_size, y = forming_column_start_y-map_size, last_y = y-map_size, tile = forming_column_tile}
+          draw_map[last_draw_map_index] = {x = x+map_size_x, y = forming_column_start_y-map_size_y, last_y = y-map_size_y, tile = forming_column_tile}
           last_draw_map_index = last_draw_map_index + 1
         end
         forming_column_start_y = y
@@ -187,27 +289,34 @@ set_callback(function()
       end
     end
     if forming_column_tile ~= nil then
-      draw_map[last_draw_map_index] = {x = x+map_size, y = forming_column_start_y-map_size, last_y = -map_size*2, tile = forming_column_tile}
+      draw_map[last_draw_map_index] = {x = x+map_size_x, y = forming_column_start_y-map_size_y, last_y = -map_size_y*2, tile = forming_column_tile}
       last_draw_map_index = last_draw_map_index + 1
     end
   end
 end, ON.GUIFRAME)
 
+
 ---@param ctx GuiDrawContext
 set_callback(function (ctx)
-  if window_open then
-  ---@param ctx GuiDrawContext
-    window_open = ctx:window("Map", -0.95, 0.74, 0.2, 0.2, true, function(ctx, pos, size)
+  if options.window_open then
+    local win = options.window_info
+    ---@param ctx GuiDrawContext
+    options.window_open = ctx:window("Map", win.x, win.y, win.w, win.h, true, function(ctx, pos, size)
       map_screen_x, map_screen_y = pos.x, pos.y
       map_screen_size = size.x
+      win.x, win.y, win.w, win.h = pos.x, pos.y, size.x, size.y
     end)
+  end
+  if options.map_type == MAP_TYPE.FOLLOW_PLAYER then
+    map_size_x = options.map_size_x
+    map_size_y = options.map_size_y
   end
   if map_screen_size == math.huge or state.screen ~= SCREEN.LEVEL then return end -- Prevent infinity error
   --render map
   local size_x, size_y = map_screen_size, (map_screen_size * 16) / 9
   local rect = AABB:new(.0, .0, .0, .0) -- Using one AABB variable for better performance
-  local sq_size_y = (size_y / (map_size * 2 + 1))
-  local sq_size_x = (size_x / (map_size * 2 + 1))
+  local sq_size_y = (size_y / (map_size_x * 2 + 1))
+  local sq_size_x = (size_x / (map_size_x * 2 + 1))
   for _, line in pairs(draw_map) do
     local color = get_tile_color(line.tile)
     rect.left, rect.top, rect.right, rect.bottom = line.x*sq_size_x, line.y*sq_size_y, line.x*sq_size_x+sq_size_x, line.last_y*sq_size_y
@@ -215,6 +324,7 @@ set_callback(function (ctx)
     ctx:draw_rect_filled(rect, 0, color)
   end
   if options.draw_border then
-    ctx:draw_rect(AABB:new(map_screen_x, map_screen_y, map_screen_x+size_x, map_screen_y-size_y), 1, 0, border_color)
+    local bottom_pos = map_screen_y - (sq_size_y * map_size_y*2)
+    ctx:draw_rect(AABB:new(map_screen_x, map_screen_y, map_screen_x+size_x, bottom_pos), 1, 0, border_color)
   end
 end, ON.GUIFRAME)
